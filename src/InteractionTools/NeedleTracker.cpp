b6e3ce73e5c2b8177d7752ea9fa0c4924fc8491e
erik pernod <erik.pernod@gmail.com>
2020-09-29 14:48:41 +0200

    [src] Add NeedleTracker class

---
+++ src/InteractionTools/NeedleTracker.cpp
@@ -0,0 +1,373 @@
+#include "NeedleTracker.h"
+#include <sofa/core/visual/VisualParams.h>
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/simulation/AnimateEndEvent.h>
+#include <sofa/helper/proximity.h>
+#include <limits>
+
+namespace sofa 
+{
+
+namespace component
+{
+
+namespace collision
+{
+int NeedleTrackerClass = core::RegisterObject("Class to track the current position of the needle tip.")
+    .add< NeedleTracker >()
+    ;
+
+
+NeedleTracker::NeedleTracker()
+    : d_drawDebug(initData(&d_drawDebug, true, "drawBB", "if true, will draw slices BB, ray and intersected triangles"))
+    , d_sliceID(initData(&d_sliceID, -1, "sliceID", "ID of the sliced where the needle tip is. -1 if none."))
+    , d_sliceName(initData(&d_sliceName, std::string("None"), "sliceName", "ID of the sliced where the needle tip is. -1 if none."))
+    , isInit(false)
+    , m_needle(NULL)
+{
+
+}
+
+
+NeedleTracker::~NeedleTracker()
+{
+
+}
+
+
+void NeedleTracker::bwdInit()
+{
+    computeSlicesBB();
+}
+
+
+void NeedleTracker::init()
+{
+    getContext()->get<MechanicalObject3>(&m_slices, core::objectmodel::Tag("slice"), core::objectmodel::BaseContext::SearchRoot);
+    if (m_slices.empty())
+        msg_error() << "No slices mechanicalObject found";
+    else {
+        msg_info() << "found Number of slices: " << m_slices.size();
+        m_min.resize(m_slices.size());
+        m_max.resize(m_slices.size());
+    }
+
+    // get needle position 
+    sofa::helper::vector<MechanicalObject3*> buffer;
+    getContext()->get<MechanicalObject3>(&buffer, core::objectmodel::Tag("needle"), core::objectmodel::BaseContext::SearchRoot);
+    if (buffer.empty())
+    {
+        msg_error() << "No needle mechanicalObject found";
+        m_needle = NULL;
+        return;
+    }
+    else 
+    {
+        std::cout << "Needle found." << std::endl;
+        m_needle = buffer[0];
+    }
+}
+
+
+void NeedleTracker::computeSlicesBB()
+{
+    for (int i = 0; i < m_slices.size(); ++i)
+    {
+        MechanicalObject3* meca = m_slices[i];
+        Coord& sliceMin = m_min[i];
+        Coord& sliceMax = m_max[i];
+
+        // init max BB values
+        sliceMin = Coord(FLT_MAX, FLT_MAX, FLT_MAX);
+        sliceMax = Coord(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+
+        size_t nbrDof = meca->getSize();
+        for (int j = 0; j < nbrDof; ++j)
+        {
+            Coord pos = Coord(meca->getPX(j), meca->getPY(j), meca->getPZ(j));
+
+            // check max values
+            for (int k = 0; k < 3; ++k)
+                if (pos[k] > sliceMax[k])
+                    sliceMax[k] = pos[k];
+
+            // check min values
+            for (int k = 0; k < 3; ++k)
+                if (pos[k] < sliceMin[k])
+                    sliceMin[k] = pos[k];
+        }
+    }
+
+    isInit = true;
+}
+
+
+std::string NeedleTracker::getPositionInSlices(const Coord& tipPosition)
+{
+    // recompute BB first:
+    computeSlicesBB();
+
+    int sliceID = -1;
+    
+    // first look at the BB
+    for (int i = 0; i < m_slices.size(); ++i) 
+    {
+        bool insideBB = testSliceBBIntersection(i, tipPosition);        
+        //std::cout << i << " -> " << insideBB << std::endl;
+        // check close intersection
+        if (insideBB)
+        {
+            bool inside = testSliceDiscretIntersection(i, tipPosition);
+            if (inside)
+            {
+                sliceID = i;
+                break;
+            }
+        }
+    }
+
+    d_sliceID.setValue(sliceID);
+    if (sliceID == -1)
+        d_sliceName.setValue(std::string("None"));
+    else
+        d_sliceName.setValue(m_slices[sliceID]->getName());
+       
+    return d_sliceName.getValue();
+}
+
+
+bool NeedleTracker::testSliceBBIntersection(int sliceID, const Coord& tipPosition)
+{
+    const Coord& min = m_min[sliceID];
+    const Coord& max = m_max[sliceID];
+    //std::cout << min << " - position: " << tipPosition << " - " << max << std::endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        if (tipPosition[i] < min[i])
+            return false;
+
+        if (tipPosition[i] > max[i])
+            return false;
+    }
+
+    return true;
+}
+
+using sofa::core::topology::BaseMeshTopology;
+bool NeedleTracker::testSliceDiscretIntersection(int sliceID, const Coord& tipPosition)
+{
+    // Todo lancé de rayon ici
+    if (sliceID >= m_slices.size()) {
+        msg_error() << "SlideId " << sliceID << " out fo bounds.";
+        return false;
+    }
+
+    MechanicalObject3* meca = m_slices[sliceID];
+    // todo use: sofa::core::topology::BaseMeshTopology* _topology = l_topology.get();
+    BaseMeshTopology* topo = meca->getContext()->getMeshTopology();
+
+    if (topo == nullptr)
+    {
+        msg_error() << "No topology found for slice: " << sliceID;
+        return false;
+    }
+
+    const BaseMeshTopology::SeqTriangles& triangles = topo->getTriangles();
+    if (triangles.empty())
+    {
+        msg_error() << "No triangles found in topology for slice: " << sliceID;
+        return false;
+    }
+
+    // compute ray direction to first triangle
+    const BaseMeshTopology::Triangle& tri0 = triangles[0];
+    Coord pos0 = Coord(0.0, 0.0, 0.0);
+    for (int i = 0; i < 3; ++i)
+    {
+        int vId = tri0[i];
+        pos0 += Coord(meca->getPX(vId), meca->getPY(vId), meca->getPZ(vId));
+    }
+    pos0 /= 3;
+    m_rayDirection = (pos0 - tipPosition);
+    //m_rayDirection = tipPosition + m_rayDirection * 100;
+    m_rayOrigin = tipPosition;    
+
+    //static sofa::helper::DistanceSegTri proximitySolver;
+    m_triPointInter.clear();
+    for (int i = 0; i < triangles.size(); ++i)
+    {
+        const BaseMeshTopology::Triangle& tri = triangles[i];
+        Vector3 out;
+        Vector3 p0 = Vector3(meca->getPX(tri[0]), meca->getPY(tri[0]), meca->getPZ(tri[0]));
+        Vector3 p1 = Vector3(meca->getPX(tri[1]), meca->getPY(tri[1]), meca->getPZ(tri[1]));
+        Vector3 p2 = Vector3(meca->getPX(tri[2]), meca->getPY(tri[2]), meca->getPZ(tri[2]));
+       
+        bool intersect = RayIntersectsTriangle(m_rayOrigin, m_rayDirection, p0, p1, p2, out);
+
+        if (intersect)
+        {
+            //std::cout << "sliceID : " << sliceID << " | triID: " << i << " pos: " << out << std::endl;
+            m_triPointInter.push_back(p0);
+            m_triPointInter.push_back(p1);
+            m_triPointInter.push_back(p2);
+        }
+    }
+
+    int nbrInter = m_triPointInter.size() / 3;
+    if (nbrInter % 2) {
+        return true;
+    }
+    else
+        return false;
+}
+
+
+
+bool NeedleTracker::RayIntersectsTriangle(const Coord& origin, const Vector3& rayDirection, const Coord& P0, const Coord& P1, const Coord& P2, Coord& outIntersection)
+{
+    const float EPSILON = 0.0000001;
+
+    Vector3 edge1, edge2, h, s, q;
+    float a, f, u, v;
+    edge1 = P1 - P0;
+    edge2 = P2 - P0;
+    h = rayDirection.cross(edge2);
+    a = edge1 * h;
+
+    if (a > -EPSILON && a < EPSILON)
+        return false;    // This ray is parallel to this triangle.
+    f = 1.0 / a;
+    s = origin - P0;
+    u = f * (s * h);
+
+    if (u < 0.0 || u > 1.0)
+        return false;
+    q = s.cross(edge1);
+    v = f * rayDirection * q;
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = f * ( edge2 * q);
+    if (t > EPSILON) // ray intersection
+    {
+        outIntersection = origin + rayDirection * t;
+        return true;
+    }
+    else // This means that there is a line intersection but not a ray intersection.
+        return false;
+}
+
+
+void NeedleTracker::handleEvent(sofa::core::objectmodel::Event* event)
+{
+    if (sofa::simulation::AnimateEndEvent::checkEventType(event))
+    {        
+        if (m_needle != NULL && m_needle->getSize() > 0) 
+        {
+            Coord tipPos = Coord(m_needle->getPX(0), m_needle->getPY(0), m_needle->getPZ(0));
+            std::string res = getPositionInSlices(tipPos);
+            //std::cout << res << std::endl;
+        }
+    }
+}
+
+
+void NeedleTracker::draw(const core::visual::VisualParams* vparams)
+{
+    if (!isInit || m_slices.empty())
+        return;
+
+    if (!d_drawDebug.getValue())
+        return;
+
+    vparams->drawTool()->setLightingEnabled(false);
+
+    // Draw BB
+    {
+        for (int i = 0; i < m_slices.size(); ++i)
+        {
+            float val = (float)i / m_slices.size();
+            sofa::defaulttype::Vec4f colorDefault = sofa::defaulttype::Vec4f(0.0f, 1.0f, 0.0f, 1.0f);
+
+            if (d_sliceID.getValue() == i)
+                colorDefault = sofa::defaulttype::Vec4f(1.0f, 0.0f, 0.0f, 1.0f);
+
+            sofa::helper::vector<Coord> vertices;
+            Coord min = m_min[i];
+            Coord max = m_max[i];
+
+            // FACE A
+            vertices.push_back(Coord(min[0], min[1], min[2]));
+            vertices.push_back(Coord(min[0], max[1], min[2]));
+
+            vertices.push_back(Coord(min[0], min[1], min[2]));
+            vertices.push_back(Coord(max[0], min[1], min[2]));
+
+            vertices.push_back(Coord(min[0], max[1], min[2]));
+            vertices.push_back(Coord(max[0], max[1], min[2]));
+
+            vertices.push_back(Coord(max[0], min[1], min[2]));
+            vertices.push_back(Coord(max[0], max[1], min[2]));
+
+            // FACE B
+            vertices.push_back(Coord(min[0], min[1], max[2]));
+            vertices.push_back(Coord(min[0], max[1], max[2]));
+
+            vertices.push_back(Coord(min[0], min[1], max[2]));
+            vertices.push_back(Coord(max[0], min[1], max[2]));
+
+            vertices.push_back(Coord(min[0], max[1], max[2]));
+            vertices.push_back(Coord(max[0], max[1], max[2]));
+
+            vertices.push_back(Coord(max[0], min[1], max[2]));
+            vertices.push_back(Coord(max[0], max[1], max[2]));
+
+            // Middle
+            vertices.push_back(Coord(min[0], min[1], min[2]));
+            vertices.push_back(Coord(min[0], min[1], max[2]));
+
+            vertices.push_back(Coord(max[0], min[1], min[2]));
+            vertices.push_back(Coord(max[0], min[1], max[2]));
+
+            vertices.push_back(Coord(min[0], max[1], min[2]));
+            vertices.push_back(Coord(min[0], max[1], max[2]));
+
+            vertices.push_back(Coord(max[0], max[1], min[2]));
+            vertices.push_back(Coord(max[0], max[1], max[2]));
+
+            vparams->drawTool()->drawLines(vertices, 1.0f, colorDefault);
+        }
+    }
+
+    // Narrow phase debug
+    if (!m_triPointInter.empty())// possible intersection detected
+    {
+        // Draw ray
+        vparams->drawTool()->drawLine(m_rayOrigin, m_rayOrigin + m_rayDirection * 100, sofa::defaulttype::Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+
+        sofa::helper::vector<Coord> vertices;
+        for (int i = 0; i < (int)(m_triPointInter.size() / 3); ++i)
+        {
+            vertices.push_back(m_triPointInter[i * 3]);
+            vertices.push_back(m_triPointInter[i * 3 + 1]);
+
+            vertices.push_back(m_triPointInter[i * 3]);
+            vertices.push_back(m_triPointInter[i * 3 + 2]);
+
+            vertices.push_back(m_triPointInter[i * 3 + 2]);
+            vertices.push_back(m_triPointInter[i * 3 + 1]);
+        }
+
+        vparams->drawTool()->drawLines(vertices, 10.0f, sofa::defaulttype::Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+    }
+
+    vparams->drawTool()->setLightingEnabled(true);
+}
+
+
+} // namespace collision
+
+} // namespace component
+
+} // namespace sofa
+
---
+++ src/InteractionTools/NeedleTracker.h
@@ -0,0 +1,98 @@
+#ifndef SOFA_INTERACTIONTOOLS_NEEDLETRACKER_H
+#define SOFA_INTERACTIONTOOLS_NEEDLETRACKER_H
+
+#include <InteractionTools/config.h>
+#include <sofa/core/behavior/BaseController.h>
+#include <sofa/defaulttype/Vec.h>
+#include <SofaBaseMechanics/MechanicalObject.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace collision
+{
+    
+using namespace sofa::defaulttype;
+
+/** Will search for MechanicalObject with Tag "slice" as target meshes.
+* Use the position of the MechanicalObject Tag as "needle".
+* At each step: 
+*  - BB of each target mesh is updated (TODO: this can be optimised)
+*  - Check if position of the needle is inside a BB: BroadPhase
+*    - if position is inside the BB mesh (could have several), cast ray from needle position to first triangle.
+*      if an odd number of triangle is intersected by the ray, needle is inside mesh otherwise outside.
+* If needle is inside a mesh, @sa d_sliceName contain the name of this mesh. If none, d_sliceName return "None".
+*/
+class SOFA_INTERACTIONTOOLS_API NeedleTracker : public sofa::core::behavior::BaseController
+{
+public:
+    typedef defaulttype::Vec3Types DataTypes;
+    typedef DataTypes::Coord Coord;
+    typedef DataTypes::Real Real;
+    typedef sofa::component::container::MechanicalObject<sofa::defaulttype::Vec3Types> MechanicalObject3;
+
+    NeedleTracker();
+    virtual ~NeedleTracker();
+
+    Data<bool> d_drawDebug; ///< if true, draw the Mesh BoundingBox, ray and triangle intersected
+    Data<int> d_sliceID; /// id of the mesh (in @sa m_slices) where the needle is. -1 if none
+    Data<std::string> d_sliceName; /// Name of the mesh where the needle is. "None" if needle is outside of all meshes.
+public:
+	/// Sofa API methods
+    virtual void init() override;
+
+    virtual void bwdInit() override;
+
+    virtual void handleEvent(sofa::core::objectmodel::Event* event) override;
+
+    //display debug draw info
+    void draw(const core::visual::VisualParams* vparams);
+
+    /// Return the number of targeted meshes
+    size_t numberOfSlice() { return m_slices.size(); }
+    /// Return the name of the Slice where the needle is. "None"if needle is outside from all meshes.
+    std::string getPositionInSlices(const Coord& tipPosition);    
+
+protected:
+    /// BroadPhase method, test needle position with slice BB
+    bool testSliceBBIntersection(int sliceID, const Coord& tipPosition);
+    /// NarrowPhase method, test needle position with slice mesh triangles
+    bool testSliceDiscretIntersection(int sliceID, const Coord& tipPosition);
+
+
+    /// Internal method to compute each slice BB
+    void computeSlicesBB();
+    /// Internal method to test the intersection between a ray and a triangle
+    bool RayIntersectsTriangle(const Coord& origin, const Vector3& rayDirection, const Coord& P0, const Coord& P1, const Coord& P2, Coord& outIntersection);
+
+
+protected:
+    /// Vector storing pointer to each mechanicalObject of the target mesh
+    sofa::helper::vector<MechanicalObject3*> m_slices;
+    /// Pointer to the MechanicalObject corresponding to the needle position
+    MechanicalObject3* m_needle;
+
+    /// Vector of min and max of the slice boudingboxes
+    sofa::helper::vector<Coord> m_min;
+    sofa::helper::vector<Coord> m_max;
+    
+    /// Origin and direction of the ray (for draw debug only)
+    Coord m_rayDirection;
+    Coord m_rayOrigin;
+
+    /// vector of triangles positions intersected by narrow phase ray.
+    sofa::helper::vector <Coord> m_triPointInter;
+
+    bool isInit;
+};
+
+} // namespace collision
+
+} // namespace component
+
+} // namespace sofa
+
+#endif //SOFA_INTERACTIONTOOLS_NEEDLETRACKER_H
