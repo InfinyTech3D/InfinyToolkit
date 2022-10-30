613090c58419a9fc499208ababfd0d0c0e7b1c13
epernod <erik.pernod@gmail.com>
2022-10-30 21:29:09 +0100

    [src] Update all file copyright header

--- src/InfinyToolkit/BruteForceFeedback.cpp
+++ src/InfinyToolkit/BruteForceFeedback.cpp
@@ -1,10 +1,25 @@
 /*****************************************************************************
- *            Copyright (C) - InfinyTech3D - All Rights Reserved             *
+ *                 - Copyright (C) - 2020 - InfinyTech3D -                   *
  *                                                                           *
- * Unauthorized copying of this file, via any medium is strictly prohibited  *
- * Proprietary and confidential.                                             *
+ * This file is part of the InfinyToolkit plugin for the SOFA framework      *
  *                                                                           *
- * Written by Erik Pernod <erik.pernod@infinytech3d.com>, October 2019       *
+ * Commercial License Usage:                                                 *
+ * Licensees holding valid commercial license from InfinyTech3D may use this *
+ * file in accordance with the commercial license agreement provided with    *
+ * the Software or, alternatively, in accordance with the terms contained in *
+ * a written agreement between you and InfinyTech3D. For further information *
+ * on the licensing terms and conditions, contact: contact@infinytech3d.com  *
+ *                                                                           *
+ * GNU General Public License Usage:                                         *
+ * Alternatively, this file may be used under the terms of the GNU General   *
+ * Public License version 3. The licenses are as published by the Free       *
+ * Software Foundation and appearing in the file LICENSE.GPL3 included in    *
+ * the packaging of this file. Please review the following information to    *
+ * ensure the GNU General Public License requirements will be met:           *
+ * https://www.gnu.org/licenses/gpl-3.0.html.                                *
+ *                                                                           *
+ * Authors: see Authors.txt                                                  *
+ * Further information: https://infinytech3d.com                             *
  ****************************************************************************/
 
 #include <InfinyToolkit/BruteForceFeedback.h>
