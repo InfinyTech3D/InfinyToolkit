72b9e30c0689bb764d76963d39ea307389f3dbf5
epernod <erik.pernod@gmail.com>
2023-11-22 14:52:37 +0100

    [MeshTools] Update NearestTexCoordsMap engine to have 2 methods, the original one using nearest point and a second that will find the 3 closest point in order to compute a barycentric interpolation

--- src/InfinyToolkit/MeshTools/NearestTexcoordsMap.h
+++ src/InfinyToolkit/MeshTools/NearestTexcoordsMap.h
@@ -26,6 +26,7 @@
 #include <InfinyToolkit/config.h>
 #include <sofa/core/DataEngine.h>
 #include <sofa/defaulttype/VecTypes.h>
+#include <sofa/core/topology/BaseMeshTopology.h>
 
 namespace sofa::infinytoolkit
 {
@@ -39,23 +40,39 @@ class SOFA_INFINYTOOLKIT_API NearestTexcoordsMap : public sofa::core::DataEngine
 public:
     SOFA_CLASS(NearestTexcoordsMap, core::DataEngine);
 
+    typedef sofa::core::topology::BaseMeshTopology::Triangle                     Triangle;
+
     NearestTexcoordsMap();
     ~NearestTexcoordsMap() override;
 
     void init() override;
     void doUpdate() override;
 
+    void draw(const core::visual::VisualParams* vparams) override;
+
+protected:
+
+
 public:
     Data< type::vector< Vec3 > > d_inputPositions; ///< Full mesh position
     Data< type::vector< Vec3 > > d_mapPositions; ///< Surface mesh position
     Data< type::vector<sofa::type::Vec2> > d_mapTexCoords; ///< Surface mesh texcoords
     Data<SReal> d_radius; ///< Radius to search corresponding fixed point if no indices are given
-
+    
+    Data<bool> d_useInterpolation;
+    Data<bool> d_drawInterpolation;
+    
     /// Full mesh texcoords
     Data< type::vector<sofa::type::Vec2> > d_outputTexCoords;
 
+    std::vector<unsigned int> m_mapPos;
+    std::vector<sofa::type::RGBAColor> m_colors;
+    
+
 protected:
     //void computeNearestPointMaps(const VecCoord& x1, const VecCoord& x2);
+    void computeMethod1();
+    void computeMethod2();
 };
 
 } //namespace sofa::component::engine
