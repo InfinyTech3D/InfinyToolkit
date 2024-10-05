f5d66a4e29014ac0d038e050ceffd9cf497a5000
epernod <erik.pernod@gmail.com>
2024-10-06 01:05:38 +0200

    [src] Add new component (wip)

---
+++ src/InfinyToolkit/MeshTools/GridBarycentersPositions.h
@@ -0,0 +1,88 @@
+/*****************************************************************************
+ *                 - Copyright (C) - 2020 - InfinyTech3D -                   *
+ *                                                                           *
+ * This file is part of the InfinyToolkit plugin for the SOFA framework      *
+ *                                                                           *
+ * Commercial License Usage:                                                 *
+ * Licensees holding valid commercial license from InfinyTech3D may use this *
+ * file in accordance with the commercial license agreement provided with    *
+ * the Software or, alternatively, in accordance with the terms contained in *
+ * a written agreement between you and InfinyTech3D. For further information *
+ * on the licensing terms and conditions, contact: contact@infinytech3d.com  *
+ *                                                                           *
+ * GNU General Public License Usage:                                         *
+ * Alternatively, this file may be used under the terms of the GNU General   *
+ * Public License version 3. The licenses are as published by the Free       *
+ * Software Foundation and appearing in the file LICENSE.GPL3 included in    *
+ * the packaging of this file. Please review the following information to    *
+ * ensure the GNU General Public License requirements will be met:           *
+ * https://www.gnu.org/licenses/gpl-3.0.html.                                *
+ *                                                                           *
+ * Authors: see Authors.txt                                                  *
+ * Further information: https://infinytech3d.com                             *
+ ****************************************************************************/
+#pragma once
+
+#include <InfinyToolkit/config.h>
+#include <sofa/core/DataEngine.h>
+#include <sofa/defaulttype/VecTypes.h>
+#include <sofa/core/topology/BaseMeshTopology.h>
+
+namespace sofa::infinytoolkit
+{
+
+using sofa::type::Vec3;
+
+/** Attach given pair of particles, projecting the positions of the second particles to the first ones.
+*/
+class SOFA_INFINYTOOLKIT_API GridBaryCentersPositions : public sofa::core::DataEngine
+{
+public:
+    SOFA_CLASS(GridBaryCentersPositions, core::DataEngine);
+
+    typedef typename core::topology::BaseMeshTopology::SeqTetrahedra SeqTetrahedra;
+    typedef typename core::topology::BaseMeshTopology::SeqHexahedra SeqHexahedra;
+
+    GridBaryCentersPositions();
+    ~GridBaryCentersPositions() override;
+
+    void init() override;
+    void doUpdate() override;
+
+    void draw(const core::visual::VisualParams* vparams) override;
+
+protected:
+    /** 
+    * 
+    **/
+    void computeSurfaceMeshGrid();
+    
+    /** 
+    *
+    */
+    void computeOutputPositions();
+
+public:
+    /// Inputs Data
+    Data< type::vector< Vec3 > > d_inputPositions; ///< Full mesh position
+    Data< SeqTetrahedra > d_tetrahedra; ///< Tetrahedra of mesh subset
+    //Data< type::vector< Vec3 > > d_mapPositions; ///< Surface mesh position
+    //Data< type::vector<sofa::type::Vec2> > d_mapTexCoords; ///< Surface mesh texcoords
+    //Data<SReal> d_radius; ///< Radius to search corresponding fixed point if no indices are given
+    Data< sofa::type::Vec< 3, int > > d_n;
+    /// Outputs Data
+    Data< type::vector< Vec3 > > d_outputPositions; ///< Full mesh texcoords
+    Data< SeqHexahedra > d_outputGrid; ///< Hexahedra of mesh subset
+
+    /// Parameters Data
+    Data<bool> d_useInterpolation; ///< Bool option to choose between nearest point or interpolation method
+    Data<bool> d_drawInterpolation; ///< Boop optio to draw the mapping computed between inputPosition and mapPosition    
+    
+private:
+    sofa::type::vector<Vec3> m_minBBoxes;
+    sofa::type::vector<Vec3> m_maxBBoxes;
+    //sofa::type::fixed_array<QuadID, 6>
+    Vec3 m_fullMin, m_fullMax;
+};
+
+} //namespace sofa::component::engine
