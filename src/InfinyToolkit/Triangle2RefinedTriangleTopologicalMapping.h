fa1c66952675147337015cd3277a9f1a97a21ad5
epernod <erik.pernod@gmail.com>
2026-02-21 00:28:56 +0100

    [src] Updated all headers: copyright and license

--- src/InfinyToolkit/Triangle2RefinedTriangleTopologicalMapping.h
+++ src/InfinyToolkit/Triangle2RefinedTriangleTopologicalMapping.h
@@ -1,24 +1,22 @@
-/******************************************************************************
-*                 SOFA, Simulation Open-Framework Architecture                *
-*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
-*                                                                             *
-* This program is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU Lesser General Public License as published by    *
-* the Free Software Foundation; either version 2.1 of the License, or (at     *
-* your option) any later version.                                             *
-*                                                                             *
-* This program is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
-* for more details.                                                           *
-*                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
-*******************************************************************************
-* Authors: The SOFA Team and external contributors (see Authors.txt)          *
-*                                                                             *
-* Contact information: contact@sofa-framework.org                             *
-******************************************************************************/
+/*****************************************************************************
+ *                - Copyright (C) 2020-Present InfinyTech3D -                *
+ *                                                                           *
+ * This file is part of the InfinyToolkit plugin for the SOFA framework.     *
+ *                                                                           *
+ * This file is dual-licensed:                                               *
+ *                                                                           *
+ * 1) Commercial License:                                                    *
+ *      This file may be used under the terms of a valid commercial license  *
+ *      agreement provided wih the software by InfinyTech3D.                 *
+ *                                                                           *
+ * 2) GNU General Public License (GPLv3) Usage                               *
+ *      Alternatively, this file may be used under the terms of the          *
+ *      GNU General Public License version 3 as published by the             *
+ *      Free Software Foundation: https://www.gnu.org/licenses/gpl-3.0.html  *
+ *                                                                           *
+ * Contact: contact@infinytech3d.com                                         *
+ * Further information: https://infinytech3d.com                             *
+ ****************************************************************************/
 #pragma once
 #include <InfinyToolkit/config.h>
 #include <sofa/core/topology/TopologicalMapping.h>
