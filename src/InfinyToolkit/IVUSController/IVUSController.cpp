3bbd400eaf9ef9a3095dbd024f2ea3e980377c71
rmolazem <ronak.molazem@infinytech3d.com>
2026-03-09 12:15:26 +0100

    Small modifs.

--- src/InfinyToolkit/IVUSController/IVUSController.cpp
+++ src/InfinyToolkit/IVUSController/IVUSController.cpp
@@ -1,3 +1,28 @@
+/*****************************************************************************
+ *                 - Copyright (C) - 2020 - InfinyTech3D -                   *
+ *                                                                           *
+ * This file is part of the InfinyToolkit plugin for the SOFA framework      *
+ *                                                                           *
+ * Commercial License Usage:                                                 *
+ * Licensees holding valid commercial license from InfinyTech3D may use this *
+ * file in accordance with the commercial license agreement provided with    *
+ * the Software or, alternatively, in accordance with the terms contained in *
+ * a written agreement between you and InfinyTech3D. For further information *
+ * on the licensing terms and conditions, contact: contact@infinytech3d.com  *
+ *                                                                           *
+ * GNU General Public License Usage:                                         *
+ * Alternatively, this file may be used under the terms of the GNU General   *
+ * Public License version 3. The licenses are as published by the Free       *
+ * Software Foundation and appearing in the file LICENSE.GPL3 included in    *
+ * the packaging of this file. Please review the following information to    *
+ * ensure the GNU General Public License requirements will be met:           *
+ * https://www.gnu.org/licenses/gpl-3.0.html.                                *
+ *                                                                           *
+ * Authors: see Authors.txt                                                  *
+ * Further information: https://infinytech3d.com                             *
+ ****************************************************************************/
+#pragma once
+
 #include <InfinyToolkit/IVUSController/IVUSController.h>
 #include< sofa/component/collision/geometry/TriangleCollisionModel.h >
 #include <sofa/core/ObjectFactory.h>
@@ -5,9 +30,6 @@
 #include<vector>
 #include <algorithm>
 #include <numbers>
-
-using sofa::defaulttype::Vec3dTypes;
-
 
 
 namespace sofa::infinytoolkit
@@ -23,10 +45,11 @@ namespace sofa::infinytoolkit
     void registerIVUSController(sofa::core::ObjectFactory* factory)
     {
         factory->registerObjects(
-            sofa::core::ObjectRegistrationData("")
+            sofa::core::ObjectRegistrationData("IVUSController")
             .add< IVUSController >()
         );
     }
+
     IVUSController::IVUSController()
         : d_N_rays(initData(&d_N_rays, (unsigned int)128, "N_rays", "Number of angular rays"))
         , d_N_depth(initData(&d_N_depth, (unsigned int)256, "N_depth", "Number of radial depth samples"))
